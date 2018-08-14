593db4ef071847a30f756d0b674f6a26d390cfc3
Tristan Brown <tgbrown@stanford.edu>
2018-08-14 02:04:40 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(i)
+
+  while i > 0
+
+    puts "#{i} SECOND(S)!"
+    i -= 1
+
+  end
+
+    "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(i)
+  while i > 0
+
+    puts "#{i} SECOND(S)!"
+    i -= 1
+    sleep 1
+
+  end
+
+    "HAPPY NEW YEAR!"
+
 end
