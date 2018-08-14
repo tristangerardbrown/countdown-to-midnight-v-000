491cd8b4dd8974500647e164e52c376e79ccc47f
Tristan Brown <tgbrown@stanford.edu>
2018-08-14 02:04:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,3 +12,16 @@ def countdown(i)
     "HAPPY NEW YEAR!"
     
 end
+
+def countdown_with_sleep(i)
+  while i > 0
+
+    puts "#{i} SECOND(S)!"
+    i -= 1
+    sleep 1
+
+  end
+
+    "HAPPY NEW YEAR!"
+    
+end
