da460cd31ad7d9afd495c1d386b9994fbc0501ac
Tristan Brown <tgbrown@stanford.edu>
2018-08-14 01:58:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(i)
-    puts "#{i} SECOND(S)!"
+    "#{i} SECOND(S)!"
   while i > 0
     i -= 1
   end
-    puts "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
 end
